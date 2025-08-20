4f90ba8743437deead199aa02a8c6918c5c5154f
guoshang-x <xzg@email.com>
2025-08-21 01:20:39 +0800

    fix：合并冲突提交

--- fork_test.c
+++ fork_test.c
@@ -2,8 +2,17 @@
 
 int main()
 {
+    uint8_t idx;
     printf("Hello World!\n");
     peinrdf();
     printf("Goodbye World-fork_test2!\n");
+    printf("Goodbye World-fork_test1!\n");
+    printf("Goodbye World-fork_test2!\n");
+    
+    return 0;
+}
+
+int test(void *p){
+
     return 0;
 }
--- fsdf.c
+++ fsdf.c
@@ -1,1 +1,7 @@
-许多
+#include "fork_test.h"
+
+int test2(void)
+{
+    test(NULL);
+    return 0;
+}
